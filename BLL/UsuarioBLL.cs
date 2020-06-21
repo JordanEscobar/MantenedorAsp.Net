5d90cc18d772148774f8f3d5310c7018019d55a1
jordan escobar soto <jord.escobar@alumnos.duoc.cl>
2020-06-21 18:21:37 -0400

    Capas creadas(DAL,BLL y la capa WEB para las vistas). Base de datos conectada a traves de Entity Framework.

---
+++ BLL/UsuarioBLL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using DAL;
+
+namespace BLL
+{
+    class UsuarioBLL
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string sexo { get; set; }
+        public Nullable<int> edad { get; set; }
+    }
+}
